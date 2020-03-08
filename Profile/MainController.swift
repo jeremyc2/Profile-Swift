417628ad00b8b5596b8d7434e957875896b1d8ef
Jeremy Chandler <goldenmeso@gmail.com>
2020-03-08 11:34:58 -0600

    First SScreen

--- Profile/MainController.swift
+++ Profile/MainController.swift
@@ -7,29 +7,74 @@
 //
 
 import UIKit
+import LBTATools
 
 class MainController: UIViewController {
+    
+    static let themeColor:UIColor = UIColor(red: 70/255.0, green: 107/255.0, blue: 217/255.0, alpha: 1.0)
+    static let themeFont:UIFont = UIFont(name: "KohinoorTelugu-Medium", size: 20)!
         
     var mainLabel:UILabel = {
         var label:UILabel = UILabel()
         
-        label.font = UIFont(name: "KohinoorTelugu-Medium", size: 20)
-        label.textColor = UIColor(red: 70/255.0, green: 107/255.0, blue: 217/255.0, alpha: 1.0)
+        label.font = themeFont
+        label.textColor = themeColor
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
     
+    var profilePic: UIImageView = {
+        let diameter = CGFloat(150)
+        
+        var iv:UIImageView = UIImageView(backgroundColor: .red)
+        
+        iv.withHeight(diameter)
+        iv.withWidth(diameter)
+        
+        iv.layer.cornerRadius = diameter / 2
+        
+        iv.layer.borderWidth = 5
+        iv.layer.borderColor = themeColor.cgColor
+        
+                
+        return iv
+    }()
+    
+    var nameInput:UITextField = {
+        var input: UITextField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        input.translatesAutoresizingMaskIntoConstraints = false
+        input.text = "Micah Smith"
+        input.font = themeFont
+        return input
+    }()
+    
+    var phoneInput:UITextField = UITextField()
+    var emailInput:UITextField = UITextField()
+    var descriptionInput:UITextField = UITextField()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(mainLabel)
+        view.addSubview(profilePic)
+        view.addSubview(nameInput)
+        view.addSubview(phoneInput)
+        view.addSubview(emailInput)
+        view.addSubview(descriptionInput)
                 
         mainLabel.text = "Edit Profile"
-                
+                        
         NSLayoutConstraint.activate([
             mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mainLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+            mainLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            
+            profilePic.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profilePic.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 30),
+
+            nameInput.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameInput.topAnchor.constraint(equalTo: profilePic.bottomAnchor, constant: 30)
         ])
         
     }
