31df41efd0f47fa0d054e510b770f9263c7003b3
Jeremy Chandler <goldenmeso@gmail.com>
2020-03-08 20:21:17 -0600

    On a roll

---
+++ Profile/DescriptorView.swift
@@ -0,0 +1,15 @@
+//
+//  DescriptorView.swift
+//  Profile
+//
+//  Created by Jeremy Ryan Chandler on 3/8/20.
+//  Copyright © 2020 Jeremy Chandler. All rights reserved.
+//
+
+import Foundation
+
+
+import SwiftUI
+struct DescriptorPreview: PreviewProvider {
+    <#fields#>
+}
